a65ae8a56bd54403903ef50858b6084ef10aa27c
k022g0134@g.neec.ac.jp <k022g0134@g.neec.ac.jp>
2023-04-28 14:18:31 +0900

    01_00

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <Novice.h>
+#include"Matrix44.h"
 
 const char kWindowTitle[] = "LE2A_09_セト_ダイヤ";
 
@@ -25,6 +26,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
+		Matrix44 orthographicMatrix = MakeOrthographicMatrix(-160.0f, 160.0f, 200.0f, 300.0f, 0.0f, 1000.0f);
+		Matrix44 perspectiveFovMatrix = MakePerspectiveFovMatrix(0.63f, 1.33f, 0.1f, 1000.0f);
+		Matrix44 viewportMatrix = MakeViewportMatrix(100.0f, 200.0f, 600.0f, 300.0f);
+
 		///
 		/// ↑更新処理ここまで
 		///
@@ -32,6 +37,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+		MatrixScreenPrintf(0, 0, orthographicMatrix, "orthographicMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 5, perspectiveFovMatrix, "perspectiveFovMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 10, viewportMatrix, "viewportMatrix");
 
 		///
 		/// ↑描画処理ここまで
