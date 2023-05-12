b676bf2b5e79724ec0304b11dab8f4e903e0a807
k022g0134@g.neec.ac.jp <k022g0134@g.neec.ac.jp>
2023-05-12 12:59:37 +0900

    MT3_01_01

---
+++ Matrix44.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+#include"Vec3.h"
+
+const int kMatrixNum = 4;
+
+typedef struct {
+	float m[kMatrixNum][kMatrixNum];
+}Matrix44;
+
+//加算
+Matrix44 Add(const Matrix44& m1, const Matrix44& m2);
+//減算
+Matrix44 Sub(const Matrix44& m1, const Matrix44& m2);
+//乗算
+Matrix44 Multiply(const Matrix44& m1, const Matrix44& m2);
+//逆行列
+Matrix44 Inverse(const Matrix44& m);
+//転置行列
+Matrix44 Transpose(const Matrix44& m);
+//単位行列
+Matrix44 MakeIdentity44();
+//平行移動行列
+Matrix44 MakeTranslateMatrix(const Vec3& translate);
+//拡大縮小行列
+Matrix44 MakeScaleMatrix(const Vec3& scale);
+//座標変換
+Vec3 Transform(const Vec3& vector, const Matrix44& matrix);
+//X軸回転行列
+Matrix44 MakerotateXMatrix(float radian);
+//Y軸回転行列
+Matrix44 MakerotateYMatrix(float radian);
+//Z軸回転行列
+Matrix44 MakerotateZMatrix(float radian);
+//3次元アフィン変換行列
+Matrix44 MakeAffineMatrix(const Vec3& scale, const Vec3& rotate, const Vec3& translate);
+//透視投影行列
+Matrix44 MakePerspectiveFovMatrix(float fovY, float aspectRatio, float nearClip, float farClip);
+//正射影行列
+Matrix44 MakeOrthographicMatrix(float left, float top, float right, float bottom, float nearClip, float farClip);
+//ビューポート変換行列
+Matrix44 MakeViewportMatrix(float left, float top, float width, float height, float minDepth = 0.0f, float maxDepth = 1.0f);
+
+void MatrixScreenPrintf(int x, int y, const Matrix44& matrix,const char* label);
--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <Novice.h>
+#include <stdint.h>
+#include <Matrix44.h>
+#include<cmath>
 
 const char kWindowTitle[] = "LE2A_09_セト_ダイヤ";
+
+const int kWindowWidth = 1280;
+const int kWindowHeight = 720;
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -11,6 +17,22 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
+
+	const float kSpeed = 1.0f;
+	const float kTriangleHalfHeight = (100.0f * std::powf(3, (1 / 2))) / 2;
+	const Vec3 kLocalViertices[3] = {
+		{0.0f,kTriangleHalfHeight,0.0f},
+		{50.0f,-kTriangleHalfHeight,0.0f},
+		{-50.0,-kTriangleHalfHeight,0.0f}
+	};
+
+	Vec3 v1 = { 1.2f,-3.9f,2.5f };
+	Vec3 v2 = { 2.8f,0.4f,-1.3f };
+
+	Vec3 rotate = {};
+	Vec3 translate = {};
+	Vec3 cameraPos = { 0.0f,0.0f,-500.0f };
+
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -25,6 +47,39 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
+		if (keys[DIK_W]) {
+			translate.z += kSpeed;
+		}
+		if (keys[DIK_S]) {
+			translate.z -= kSpeed;
+		}
+		if (keys[DIK_D]) {
+			translate.x += kSpeed;
+		}
+		if (keys[DIK_A]) {
+			translate.x -= kSpeed;
+		}
+
+		if (translate.z <= cameraPos.z) {
+			translate.z = cameraPos.z + 1.0f;
+		}
+
+		rotate.y += 0.05f;
+
+		Vec3 cross = Cross(v1, v2);
+
+		Matrix44 worldMarix = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, rotate, translate);
+		Matrix44 cameraMatrix = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, { 0.0f,0.0f,0.0f }, cameraPos);
+		Matrix44 viewMatrix = Inverse(cameraMatrix);
+		Matrix44 projectionMatrix = MakePerspectiveFovMatrix(0.45f, float(kWindowWidth) / float(kWindowHeight), 0.1f, 100.0f);
+		Matrix44 worldViewProjectionMatrix = Multiply(worldMarix, Multiply(viewMatrix, projectionMatrix));
+		Matrix44 viewportMatrix = MakeViewportMatrix(0.0f, 0.0f, float(kWindowWidth), float(kWindowHeight));
+		Vec3 screenVertices[3];
+		for (uint32_t i = 0; i < 3; i++) {
+			Vec3 ndcVertex = Transform(kLocalViertices[i], worldViewProjectionMatrix);
+			screenVertices[i] = Transform(ndcVertex, viewportMatrix);
+		}
+
 		///
 		/// ↑更新処理ここまで
 		///
@@ -32,6 +87,14 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+		Novice::DrawTriangle(
+			int(screenVertices[0].x), int(screenVertices[0].y),
+			int(screenVertices[1].x), int(screenVertices[1].y),
+			int(screenVertices[2].x), int(screenVertices[2].y),
+			RED, kFillModeSolid);
+
+		VectorScreenPrintf(0, 0, cross, "Cross");
 
 		///
 		/// ↑描画処理ここまで
