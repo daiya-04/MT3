83db16e0cb70aab2aa7c7f0e837e65b411a663c4
k022g0134@g.neec.ac.jp <k022g0134@g.neec.ac.jp>
2023-06-23 02:22:09 +0900

    LineとRayとの衝突判定追加

--- Hit.cpp
+++ Hit.cpp
@@ -44,6 +44,35 @@ bool IsCollision(const Segment& segment, const Plane& plane) {
 	return false;
 }
 
+bool IsCollision(const Line& line, const Plane& plane) {
+
+	float dot = Dot(plane.normal, line.diff);
+
+	if (dot == 0.0f) {
+		return false;
+	}
+
+	//float t = (plane.distance - Dot(line.origin, plane.normal)) / dot;
+
+	return true;
+}
+
+bool IsCollision(const Ray& ray, const Plane& plane) {
+
+	float dot = Dot(plane.normal, ray.diff);
+
+	if (dot == 0.0f) {
+		return false;
+	}
+
+	float t = (plane.distance - Dot(ray.origin, plane.normal)) / dot;
+
+	if (t >= 0.0f) {
+		return true;
+	}
+	return false;
+}
+
 bool IsCollision(const Triangle& triangle, const Segment& segment) {
 
 	Vec3 v01 = Sub(triangle.vertices[1], triangle.vertices[0]);
@@ -54,6 +83,10 @@ bool IsCollision(const Triangle& triangle, const Segment& segment) {
 	float distance = Dot(triangle.vertices[0], normal);
 
 	float dot = Dot(normal, segment.diff);
+
+	if (dot == 0.0f) {
+		return false;
+	}
 
 	float t = (distance - Dot(segment.origin, normal)) / dot;
 
@@ -70,7 +103,77 @@ bool IsCollision(const Triangle& triangle, const Segment& segment) {
 	if (Dot(cross01, normal) >= 0.0f &&
 		Dot(cross12, normal) >= 0.0f &&
 		Dot(cross20, normal) >= 0.0f) {
-		if (IsCollision(segment, { normal,distance })) {
+		if (0.0f <= t && t <= 1.0f) {
+			return true;
+		}
+	}
+	return false;
+}
+
+bool IsCollision(const Triangle& triangle, const Line& line) {
+	Vec3 v01 = Sub(triangle.vertices[1], triangle.vertices[0]);
+	Vec3 v12 = Sub(triangle.vertices[2], triangle.vertices[1]);
+	Vec3 v20 = Sub(triangle.vertices[0], triangle.vertices[2]);
+
+	Vec3 normal = Normalize(Cross(v01, v12));
+	float distance = Dot(triangle.vertices[0], normal);
+
+	float dot = Dot(normal, line.diff);
+
+	if (dot == 0.0f) {
+		return false;
+	}
+
+	float t = (distance - Dot(line.origin, normal)) / dot;
+
+	Vec3 p = Add(line.origin, Mul(t, line.diff));
+
+	Vec3 v0p = Sub(p, triangle.vertices[0]);
+	Vec3 v1p = Sub(p, triangle.vertices[1]);
+	Vec3 v2p = Sub(p, triangle.vertices[2]);
+
+	Vec3 cross01 = Cross(v01, v1p);
+	Vec3 cross12 = Cross(v12, v2p);
+	Vec3 cross20 = Cross(v20, v0p);
+
+	if (Dot(cross01, normal) >= 0.0f &&
+		Dot(cross12, normal) >= 0.0f &&
+		Dot(cross20, normal) >= 0.0f) {
+		return true;
+	}
+	return false;
+}
+
+bool IsCollision(const Triangle& triangle, const Ray& ray) {
+	Vec3 v01 = Sub(triangle.vertices[1], triangle.vertices[0]);
+	Vec3 v12 = Sub(triangle.vertices[2], triangle.vertices[1]);
+	Vec3 v20 = Sub(triangle.vertices[0], triangle.vertices[2]);
+
+	Vec3 normal = Normalize(Cross(v01, v12));
+	float distance = Dot(triangle.vertices[0], normal);
+
+	float dot = Dot(normal, ray.diff);
+
+	if (dot == 0.0f) {
+		return false;
+	}
+
+	float t = (distance - Dot(ray.origin, normal)) / dot;
+
+	Vec3 p = Add(ray.origin, Mul(t, ray.diff));
+
+	Vec3 v0p = Sub(p, triangle.vertices[0]);
+	Vec3 v1p = Sub(p, triangle.vertices[1]);
+	Vec3 v2p = Sub(p, triangle.vertices[2]);
+
+	Vec3 cross01 = Cross(v01, v1p);
+	Vec3 cross12 = Cross(v12, v2p);
+	Vec3 cross20 = Cross(v20, v0p);
+
+	if (Dot(cross01, normal) >= 0.0f &&
+		Dot(cross12, normal) >= 0.0f &&
+		Dot(cross20, normal) >= 0.0f) {
+		if (0.0f <= t) {
 			return true;
 		}
 	}
--- Hit.h
+++ Hit.h
@@ -7,7 +7,59 @@
 
 Vec3 ClosestPoint(const Vec3& point, const Segment& segment);
 
+/// <summary>
+/// 球体同士の衝突判定
+/// </summary>
+/// <param name="s1">球体A</param>
+/// <param name="s2">球体B</param>
+/// <returns></returns>
 bool IsCollision(const Sphere& s1, const Sphere& s2);
+/// <summary>
+/// 平面と球体との衝突判定
+/// </summary>
+/// <param name="plane">平面</param>
+/// <param name="sphere">球体</param>
+/// <returns></returns>
 bool IsCollision(const Plane& plane, const Sphere& sphere);
+/// <summary>
+/// 線分と平面との衝突判定
+/// </summary>
+/// <param name="segment">線分</param>
+/// <param name="plane">平面</param>
+/// <returns></returns>
 bool IsCollision(const Segment& segment, const Plane& plane);
+/// <summary>
+/// 直線と平面との衝突判定
+/// </summary>
+/// <param name="line">直線</param>
+/// <param name="plane">平面</param>
+/// <returns></returns>
+bool IsCollision(const Line& line, const Plane& plane);
+/// <summary>
+/// 半直線と平面との衝突判定
+/// </summary>
+/// <param name="ray">半直線</param>
+/// <param name="plane">平面</param>
+/// <returns></returns>
+bool IsCollision(const Ray& ray, const Plane& plane);
+/// <summary>
+/// 三角形と線分の衝突判定
+/// </summary>
+/// <param name="triangle">三角形</param>
+/// <param name="segment">平面</param>
+/// <returns></returns>
 bool IsCollision(const Triangle& triangle, const Segment& segment);
+/// <summary>
+/// 三角形と直線との衝突判定
+/// </summary>
+/// <param name="triangle">三角形</param>
+/// <param name="line">直線</param>
+/// <returns></returns>
+bool IsCollision(const Triangle& triangle, const Line& line);
+/// <summary>
+/// 三角形と半直線との衝突判定
+/// </summary>
+/// <param name="triangle">三角形</param>
+/// <param name="ray">半直線</param>
+/// <returns></returns>
+bool IsCollision(const Triangle& triangle, const Ray& ray);
