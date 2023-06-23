a19a3cce7d068315b5c3a8226357daa258732c02
k022g0134@g.neec.ac.jp <k022g0134@g.neec.ac.jp>
2023-06-23 11:38:13 +0900

    MT3_02_05

---
+++ Hit.cpp
@@ -0,0 +1,191 @@
+﻿#include "Hit.h"
+#include <cmath>
+#include "imgui.h"
+
+Vec3 ClosestPoint(const Vec3& point, const Segment& segment) {
+	return Add(segment.origin, Project(Sub(point, segment.origin), segment.diff));
+}
+
+bool IsCollision(const Sphere& s1, const Sphere& s2) {
+
+	float distance = Length(Sub(s1.center, s2.center));
+
+	if (distance <= s1.radius + s2.radius) {
+		return true;
+	}
+
+	return false;
+}
+
+bool IsCollision(const Plane& plane, const Sphere& sphere) {
+
+	float distance = std::fabs(Dot(plane.normal, sphere.center) - plane.distance);
+
+	if (distance <= sphere.radius) {
+		return true;
+	}
+
+	return false;
+}
+
+bool IsCollision(const Segment& segment, const Plane& plane) {
+
+	float dot = Dot(plane.normal, segment.diff);
+
+	if (dot == 0.0f) {
+		return false;
+	}
+
+	float t = (plane.distance - Dot(segment.origin, plane.normal)) / dot;
+
+	if (t >= 0.0f && t <= 1.0f) {
+		return true;
+	}
+	return false;
+}
+
+bool IsCollision(const Line& line, const Plane& plane) {
+
+	float dot = Dot(plane.normal, line.diff);
+
+	if (dot == 0.0f) {
+		return false;
+	}
+
+	//float t = (plane.distance - Dot(line.origin, plane.normal)) / dot;
+
+	return true;
+}
+
+bool IsCollision(const Ray& ray, const Plane& plane) {
+
+	float dot = Dot(plane.normal, ray.diff);
+
+	if (dot == 0.0f) {
+		return false;
+	}
+
+	float t = (plane.distance - Dot(ray.origin, plane.normal)) / dot;
+
+	if (t >= 0.0f) {
+		return true;
+	}
+	return false;
+}
+
+bool IsCollision(const Triangle& triangle, const Segment& segment) {
+
+	Vec3 v01 = Sub(triangle.vertices[1], triangle.vertices[0]);
+	Vec3 v12 = Sub(triangle.vertices[2], triangle.vertices[1]);
+	Vec3 v20 = Sub(triangle.vertices[0], triangle.vertices[2]);
+
+	Vec3 normal = Normalize(Cross(v01, v12));
+	float distance = Dot(triangle.vertices[0], normal);
+
+	float dot = Dot(normal, segment.diff);
+
+	if (dot == 0.0f) {
+		return false;
+	}
+
+	float t = (distance - Dot(segment.origin, normal)) / dot;
+
+	Vec3 p = Add(segment.origin, Mul(t, segment.diff));
+
+	Vec3 v0p = Sub(p, triangle.vertices[0]);
+	Vec3 v1p = Sub(p, triangle.vertices[1]);
+	Vec3 v2p = Sub(p, triangle.vertices[2]);
+
+	Vec3 cross01 = Cross(v01, v1p);
+	Vec3 cross12 = Cross(v12, v2p);
+	Vec3 cross20 = Cross(v20, v0p);
+
+	if (Dot(cross01, normal) >= 0.0f &&
+		Dot(cross12, normal) >= 0.0f &&
+		Dot(cross20, normal) >= 0.0f) {
+		if (0.0f <= t && t <= 1.0f) {
+			return true;
+		}
+	}
+	return false;
+}
+
+bool IsCollision(const Triangle& triangle, const Line& line) {
+	Vec3 v01 = Sub(triangle.vertices[1], triangle.vertices[0]);
+	Vec3 v12 = Sub(triangle.vertices[2], triangle.vertices[1]);
+	Vec3 v20 = Sub(triangle.vertices[0], triangle.vertices[2]);
+
+	Vec3 normal = Normalize(Cross(v01, v12));
+	float distance = Dot(triangle.vertices[0], normal);
+
+	float dot = Dot(normal, line.diff);
+
+	if (dot == 0.0f) {
+		return false;
+	}
+
+	float t = (distance - Dot(line.origin, normal)) / dot;
+
+	Vec3 p = Add(line.origin, Mul(t, line.diff));
+
+	Vec3 v0p = Sub(p, triangle.vertices[0]);
+	Vec3 v1p = Sub(p, triangle.vertices[1]);
+	Vec3 v2p = Sub(p, triangle.vertices[2]);
+
+	Vec3 cross01 = Cross(v01, v1p);
+	Vec3 cross12 = Cross(v12, v2p);
+	Vec3 cross20 = Cross(v20, v0p);
+
+	if (Dot(cross01, normal) >= 0.0f &&
+		Dot(cross12, normal) >= 0.0f &&
+		Dot(cross20, normal) >= 0.0f) {
+		return true;
+	}
+	return false;
+}
+
+bool IsCollision(const Triangle& triangle, const Ray& ray) {
+	Vec3 v01 = Sub(triangle.vertices[1], triangle.vertices[0]);
+	Vec3 v12 = Sub(triangle.vertices[2], triangle.vertices[1]);
+	Vec3 v20 = Sub(triangle.vertices[0], triangle.vertices[2]);
+
+	Vec3 normal = Normalize(Cross(v01, v12));
+	float distance = Dot(triangle.vertices[0], normal);
+
+	float dot = Dot(normal, ray.diff);
+
+	if (dot == 0.0f) {
+		return false;
+	}
+
+	float t = (distance - Dot(ray.origin, normal)) / dot;
+
+	Vec3 p = Add(ray.origin, Mul(t, ray.diff));
+
+	Vec3 v0p = Sub(p, triangle.vertices[0]);
+	Vec3 v1p = Sub(p, triangle.vertices[1]);
+	Vec3 v2p = Sub(p, triangle.vertices[2]);
+
+	Vec3 cross01 = Cross(v01, v1p);
+	Vec3 cross12 = Cross(v12, v2p);
+	Vec3 cross20 = Cross(v20, v0p);
+
+	if (Dot(cross01, normal) >= 0.0f &&
+		Dot(cross12, normal) >= 0.0f &&
+		Dot(cross20, normal) >= 0.0f) {
+		if (0.0f <= t) {
+			return true;
+		}
+	}
+	return false;
+}
+
+bool IsCollision(const AABB& aabb1, const AABB& aabb2) {
+	if ((aabb1.min.x <= aabb2.max.x && aabb1.max.x >= aabb2.min.x) &&
+		(aabb1.min.y <= aabb2.max.y && aabb1.max.y >= aabb2.min.y) &&
+		(aabb1.min.z <= aabb2.max.z && aabb1.max.z >= aabb2.min.z)) {
+		return true;
+	}
+
+	return false;
+}
--- main.cpp
+++ main.cpp
@@ -1,4 +1,10 @@
 #include <Novice.h>
+#include "Vec3.h"
+#include "Matrix44.h"
+#include "Draw.h"
+#include "Hit.h"
+#include "imgui.h"
+#include <cmath>
 
 const char kWindowTitle[] = "LE2A_09_セト_ダイヤ";
 
@@ -11,6 +17,25 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
+
+	const int kWindowWidth = 1280;
+	const int kWindowHeight = 720;
+
+	AABB aabb1 = {
+		{-0.5f,-0.5f,-0.5f},
+		{0.0f,0.0f,0.0f}
+	};
+
+	AABB aabb2 = {
+		{0.2f,0.2f,0.2f},
+		{1.0f,1.0f,1.0f}
+	};
+
+	uint32_t color = 0xFFFFFFFF;
+	
+
+	Vec3 cameraTranslate = { 0.0f,1.9f,-6.49f };
+	Vec3 cameraRotate = { 0.26f,0.0f,0.0f };
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -25,6 +50,44 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
+		
+
+		aabb1.min.x = (std::min)(aabb1.min.x, aabb1.max.x);
+		aabb1.max.x = (std::max)(aabb1.min.x, aabb1.max.x);
+		aabb1.min.y = (std::min)(aabb1.min.y, aabb1.max.y);
+		aabb1.max.y = (std::max)(aabb1.min.y, aabb1.max.y);
+		aabb1.min.z = (std::min)(aabb1.min.z, aabb1.max.z);
+		aabb1.max.z = (std::max)(aabb1.min.z, aabb1.max.z);
+
+		aabb2.min.x = (std::min)(aabb2.min.x, aabb2.max.x);
+		aabb2.max.x = (std::max)(aabb2.min.x, aabb2.max.x);
+		aabb2.min.y = (std::min)(aabb2.min.y, aabb2.max.y);
+		aabb2.max.y = (std::max)(aabb2.min.y, aabb2.max.y);
+		aabb2.min.z = (std::min)(aabb2.min.z, aabb2.max.z);
+		aabb2.max.z = (std::max)(aabb2.min.z, aabb2.max.z);
+
+		if (IsCollision(aabb1, aabb2)) {
+			color = 0xFF0000FF;
+		}else {
+			color = 0xFFFFFFFF;
+		}
+
+
+		Matrix44 cameraMatrix = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, cameraRotate, cameraTranslate);
+		Matrix44 viewMatrix = Inverse(cameraMatrix);
+		Matrix44 projectionMatrix = MakePerspectiveFovMatrix(0.45f, float(kWindowWidth) / float(kWindowHeight), 0.1f, 100.0f);
+		Matrix44 viewProjectionMatrix = Multiply(viewMatrix, projectionMatrix);
+		Matrix44 viewportMatrix = MakeViewportMatrix(0.0f, 0.0f, float(kWindowWidth), float(kWindowHeight));
+
+		ImGui::Begin("Window");
+		ImGui::DragFloat3("CameraTranslate", &cameraTranslate.x, 0.01f);
+		ImGui::DragFloat3("CameraRotate", &cameraRotate.x, 0.01f);
+		ImGui::DragFloat3("aabb1.min", &aabb1.min.x, 0.01f);
+		ImGui::DragFloat3("aabb1.max", &aabb1.max.x, 0.01f);
+		ImGui::DragFloat3("aabb2.min", &aabb2.min.x, 0.01f);
+		ImGui::DragFloat3("aabb2.max", &aabb2.max.x, 0.01f);
+		ImGui::End();
+
 		///
 		/// ↑更新処理ここまで
 		///
@@ -32,6 +95,11 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+		DrawGrid(viewProjectionMatrix, viewportMatrix);
+
+		DrawAABB(aabb1, viewProjectionMatrix, viewportMatrix, color);;
+		DrawAABB(aabb2, viewProjectionMatrix, viewportMatrix, 0xFFFFFFFF);
 
 		///
 		/// ↑描画処理ここまで
