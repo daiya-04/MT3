cb793001b064c47bd77539ee179ebebddec1f1a4
k022g0134@g.neec.ac.jp <k022g0134@g.neec.ac.jp>
2023-07-14 12:52:37 +0900

    MT3_03_00

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
 #include <Novice.h>
+#include "Vec3.h"
+#include "Matrix44.h"
+#include "Draw.h"
+#include "imgui.h"
 
 const char kWindowTitle[] = "LE2A_09_セト_ダイヤ";
 
@@ -11,6 +15,18 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
+
+	const int kWindowWidth = 1280;
+	const int kWindowHeight = 720;
+
+	Vec3 controlPoints[3] = {
+		{-0.8f,0.58f,1.0f},
+		{1.76f,1.0f,-0.3f},
+		{0.94f,-0.7f,2.3f}
+	};
+
+	Vec3 cameraTranslate = { 0.0f,1.9f,-6.49f };
+	Vec3 cameraRotate = { 0.26f,0.0f,0.0f };
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -25,6 +41,21 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
+
+		Matrix44 cameraMatrix = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, cameraRotate, cameraTranslate);
+		Matrix44 viewMatrix = cameraMatrix.Inverse();
+		Matrix44 projectionMatrix = MakePerspectiveFovMatrix(0.45f, float(kWindowWidth) / float(kWindowHeight), 0.1f, 100.0f);
+		Matrix44 viewProjectionMatrix = viewMatrix * projectionMatrix;
+		Matrix44 viewportMatrix = MakeViewportMatrix(0.0f, 0.0f, float(kWindowWidth), float(kWindowHeight));
+
+		ImGui::Begin("Window");
+		ImGui::DragFloat3("CameraTranslate", &cameraTranslate.x, 0.01f);
+		ImGui::DragFloat3("CameraRotate", &cameraRotate.x, 0.01f);
+		ImGui::DragFloat3("controlPoints0", &controlPoints[0].x, 0.01f);
+		ImGui::DragFloat3("controlPoints1", &controlPoints[1].x, 0.01f);
+		ImGui::DragFloat3("controlPoints2", &controlPoints[2].x, 0.01f);
+		ImGui::End();
+
 		///
 		/// ↑更新処理ここまで
 		///
@@ -32,6 +63,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+		DrawGrid(viewProjectionMatrix, viewportMatrix);
+		
+		DrawBezier(controlPoints[0], controlPoints[1], controlPoints[2], viewProjectionMatrix, viewportMatrix, 0x0000FFFF);
+
 
 		///
 		/// ↑描画処理ここまで
