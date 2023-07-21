abf19b6fb42708ec3a252681ba582d7c382df8e8
k022g0134@g.neec.ac.jp <k022g0134@g.neec.ac.jp>
2023-07-21 14:00:58 +0900

    MT3_03_02

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <Novice.h>
+#include "Vec3.h"
+#include "Matrix44.h"
+#include "imgui.h"
 
 const char kWindowTitle[] = "LE2A_09_セト_ダイヤ";
 
@@ -24,6 +27,31 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓更新処理ここから
 		///
+
+		Vec3 a{ 0.2f,1.0f,0.0f };
+		Vec3 b{ 2.4f,3.1f,1.2f };
+		Vec3 c = a + b;
+		Vec3 d = a - b;
+		Vec3 e = a * 2.4f;
+		Vec3 rotate{ 0.4f,1.43f,-0.8f };
+		Matrix44 rotateXMatrix = MakeRotateXMatrix(rotate.x);
+		Matrix44 rotateYMatrix = MakeRotateYMatrix(rotate.y);
+		Matrix44 rotateZMatrix = MakeRotateZMatrix(rotate.z);
+
+		Matrix44 rotateMatrix = rotateXMatrix * rotateYMatrix * rotateZMatrix;
+
+		ImGui::Begin("Window");
+		ImGui::Text("c:%f, %f, %f", c.x, c.y, c.z);
+		ImGui::Text("d:%f, %f, %f", d.x, d.y, d.z);
+		ImGui::Text("e:%f, %f, %f", e.x, e.y, e.z);
+		ImGui::Text(
+			"matrix:\n%f, %f, %f, %f\n%f, %f, %f, %f\n%f, %f, %f, %f\n%f, %f, %f, %f\n",
+			rotateMatrix.m[0][0], rotateMatrix.m[0][1], rotateMatrix.m[0][2], rotateMatrix.m[0][3],
+			rotateMatrix.m[1][0], rotateMatrix.m[1][1], rotateMatrix.m[1][2], rotateMatrix.m[1][3],
+			rotateMatrix.m[2][0], rotateMatrix.m[2][1], rotateMatrix.m[2][2], rotateMatrix.m[2][3],
+			rotateMatrix.m[3][0], rotateMatrix.m[3][1], rotateMatrix.m[3][2], rotateMatrix.m[3][3]
+		);
+		ImGui::End();
 
 		///
 		/// ↑更新処理ここまで
