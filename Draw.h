fdd96234864a8fe1098f2378eb3eec7b6309a495
k022g0134@g.neec.ac.jp <k022g0134@g.neec.ac.jp>
2023-08-08 17:33:26 +0900

    MT3_04_02

---
+++ Draw.h
@@ -0,0 +1,90 @@
+﻿#pragma once
+#include "Vec3.h"
+#include "Matrix44.h"
+
+const int kColumnWidht = 60;
+const int kRowHeight = 20;
+const int kDivitionNum = 16;
+
+//線分
+typedef struct {
+	Vec3 origin; //始点
+	Vec3 diff;   //終点との差分ベクトル
+}Segment;
+
+//直線
+typedef struct {
+	Vec3 origin; //始点
+	Vec3 diff;   //終点との差分ベクトル
+}Line;
+
+//半直線
+typedef struct {
+	Vec3 origin; //始点
+	Vec3 diff;   //終点との差分ベクトル
+}Ray;
+
+//球
+typedef struct {
+	Vec3 center; //中心点
+	float radius; //半径
+}Sphere;
+
+//平面
+typedef struct {
+	Vec3 normal; //法線
+	float distance; //原点からの距離
+}Plane;
+
+//三角形
+typedef struct {
+	Vec3 vertices[3]; //頂点
+}Triangle;
+
+typedef struct {
+	Vec3 min;
+	Vec3 max;
+}AABB;
+
+typedef struct {
+	Vec3 center;
+	Vec3 orientation[3];
+	Vec3 size;
+}OBB;
+
+typedef struct {
+	//アンカー。固定された端の位置
+	Vec3 anchor;
+	float naturalLegth;  //自然長
+	float stiffness;     //剛性。ばね定数k
+	float dampingCoefficient; //減衰係数
+}Spring;
+
+typedef struct {
+	Vec3 position;
+	Vec3 velocity;
+	Vec3 acceleration;
+	float mass;
+	float radius;
+	uint32_t color;
+}Ball;
+
+typedef struct {
+	Vec3 anchor;  //アンカーポイント。固定された橋の位置
+	float length; //紐の長さ
+	float angle;  //現在の角度
+	float anglarVelocity; //角速度ω
+	float anglarAcceleration; //角加速度
+}Pendulum;
+
+void DrawGrid(const Matrix44& viewProjectionMatrix, const Matrix44& viewportMatrix);
+void DrawSphere(const Sphere& sphere, const Matrix44& viewProjectionMatrix, const Matrix44& viewportMatrix, uint32_t color);
+void DrawPlane(const Plane& plane, const Matrix44& viewProjectionMatrix, const Matrix44& viewportMatrix, uint32_t color);
+void DrawTriangle(const Triangle& triangle, const Matrix44& viewProjectionMatrix, const Matrix44& viewportMatrix, uint32_t color);
+void DrawAABB(const AABB& aabb, const Matrix44& viewProjectionMatrix, const Matrix44& viewportMatrix, uint32_t color);
+void DrawBezier(const Vec3& controlPoint0, const Vec3& controlPoint1, const Vec3& controlPoint2,
+	const Matrix44& viewProjectionMatrix, const Matrix44& viewportMatrix, uint32_t color);
+void DrawOBB(const OBB& obb, const Matrix44& viewProjectionMatrix, const Matrix44& viewportMatrix, uint32_t color);
+
+void MatrixScreenPrintf(int x, int y, const Matrix44& matrix, const char* label);
+void VectorScreenPrintf(int x, int y, const Vec3& v, const char* label);
--- main.cpp
+++ main.cpp
@@ -1,4 +1,10 @@
 #include <Novice.h>
+#include "Vec3.h"
+#include "Matrix44.h"
+#include "Draw.h"
+#include "imgui.h"
+#include <cmath>
+
 
 const char kWindowTitle[] = "LE2A_09_セト_ダイヤ";
 
@@ -11,6 +17,33 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
+
+	const int kWindowWidth = 1280;
+	const int kWindowHeight = 720;
+
+	Pendulum pendulum = {
+		.anchor = {0.0f,1.0f,0.0f},
+		.length = 0.8f,
+		.angle = 0.7f,
+		.anglarVelocity = 0.0f,
+		.anglarAcceleration = 0.0f
+	};
+
+	Sphere sphere = {
+		{0.0f,0.0f,0.0f},
+		0.05f
+	};
+
+	//振り子の先端の位置
+	Vec3 point{ 0.0f,pendulum.anchor.y - pendulum.length,0.0f };
+
+	float deltaTime = 1.0f / 60.0f;
+	float gravityAcceleration = 9.8f;
+
+	Vec3 cameraTranslate = { 0.0f,0.0f,-6.49f };
+	Vec3 cameraRotate = { 0.26f,0.0f,0.0f };
+
+	bool startFlag = false;
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -25,6 +58,42 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
+		if (startFlag == true) {
+			pendulum.anglarAcceleration = -(gravityAcceleration / pendulum.length) * std::sinf(pendulum.angle);
+			pendulum.anglarVelocity += pendulum.anglarAcceleration * deltaTime;
+			pendulum.angle += pendulum.anglarVelocity * deltaTime;
+		}
+
+		point.x = pendulum.anchor.x + std::sinf(pendulum.angle) * pendulum.length;
+		point.y = pendulum.anchor.y - std::cosf(pendulum.angle) * pendulum.length;
+		point.z = pendulum.anchor.z;
+
+		Matrix44 pointWorldMatrix = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, {}, point);
+		Matrix44 sphereWorldMatrix = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, {0.0f,0.0f,pendulum.angle }, { 0.0f,-0.05f,0.0f }) * pointWorldMatrix;
+
+		point = { pointWorldMatrix.m[3][0],pointWorldMatrix.m[3][1] ,pointWorldMatrix.m[3][2] };
+		sphere.center = { sphereWorldMatrix.m[3][0],sphereWorldMatrix.m[3][1] ,sphereWorldMatrix.m[3][2] };
+
+
+		Matrix44 cameraMatrix = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, cameraRotate, cameraTranslate);
+		Matrix44 viewMatrix = cameraMatrix.Inverse();
+		Matrix44 projectionMatrix = MakePerspectiveFovMatrix(0.45f, float(kWindowWidth) / float(kWindowHeight), 0.1f, 100.0f);
+		Matrix44 viewProjectionMatrix = viewMatrix * projectionMatrix;
+		Matrix44 viewportMatrix = MakeViewportMatrix(0.0f, 0.0f, float(kWindowWidth), float(kWindowHeight));
+
+		ImGui::Begin("Camera");
+		ImGui::DragFloat3("CameraTranslate", &cameraTranslate.x, 0.01f);
+		ImGui::DragFloat3("CameraRotate", &cameraRotate.x, 0.01f);
+		ImGui::End();
+
+		ImGui::Begin("Window");
+		if (ImGui::Button("Start")) {
+			startFlag = true;
+		}
+		ImGui::DragFloat3("Sphere center", &sphere.center.x, 0.01f);
+		ImGui::DragFloat("angle", &pendulum.angle, 0.01f);
+		ImGui::End();
+
 		///
 		/// ↑更新処理ここまで
 		///
@@ -32,6 +101,15 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+		DrawGrid(viewProjectionMatrix, viewportMatrix);
+
+		DrawSphere(sphere, viewProjectionMatrix, viewportMatrix, 0xFFFFFFFF);
+
+		Vec3 start = Transform(pendulum.anchor, viewProjectionMatrix * viewportMatrix);
+		Vec3 end = Transform(point, viewProjectionMatrix * viewportMatrix);
+		Novice::DrawLine((int)start.x, (int)start.y, (int)end.x, (int)end.y, 0xFFFFFFFF);
+
 
 		///
 		/// ↑描画処理ここまで
