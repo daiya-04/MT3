7632693f404d092a570fa209a91e083373a82f52
k022g0134@g.neec.ac.jp <k022g0134@g.neec.ac.jp>
2023-08-15 00:02:13 +0900

    MT3_02_09

--- main.cpp
+++ main.cpp
@@ -1,4 +1,9 @@
 #include <Novice.h>
+#include "Vec3.h"
+#include "Matrix44.h"
+#include "Draw.h"
+#include "Hit.h"
+#include "imgui.h"
 
 const char kWindowTitle[] = "LE2A_09_セト_ダイヤ";
 
@@ -11,6 +16,29 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
+
+	const int kWindowWidth = 1280;
+	const int kWindowHeight = 720;
+
+	Vec3 rotate{};
+
+	OBB obb = {
+		.center = {-1.0f,0.0f,0.0f},
+		.orientation = {{1.0f,0.0f,0.0f},
+						{0.0f,1.0f,0.0f},
+						{0.0f,0.0f,1.0f}},
+		.size = {0.5f,0.5f,0.5f}
+	};
+
+	Segment segment = {
+		.origin = {-0.8f,-0.3f,0.0f},
+		.diff = {0.5f,0.5f,0.5f}
+	};
+
+	uint32_t color = 0u;
+
+	Vec3 cameraTranslate = { 0.0f,0.0f,-6.49f };
+	Vec3 cameraRotate = { 0.26f,0.0f,0.0f };
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -25,6 +53,47 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
+		Matrix44 rotateMatrix = MakeRotateXMatrix(rotate.x) * MakeRotateYMatrix(rotate.y) * MakeRotateZMatrix(rotate.z);
+
+		obb.orientation[0] = { rotateMatrix.m[0][0],rotateMatrix.m[0][1] ,rotateMatrix.m[0][2] };
+		obb.orientation[1] = { rotateMatrix.m[1][0],rotateMatrix.m[1][1] ,rotateMatrix.m[1][2] };
+		obb.orientation[2] = { rotateMatrix.m[2][0],rotateMatrix.m[2][1] ,rotateMatrix.m[2][2] };
+		
+
+		Matrix44 cameraMatrix = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, cameraRotate, cameraTranslate);
+		Matrix44 viewMatrix = cameraMatrix.Inverse();
+		Matrix44 projectionMatrix = MakePerspectiveFovMatrix(0.45f, float(kWindowWidth) / float(kWindowHeight), 0.1f, 100.0f);
+		Matrix44 viewProjectionMatrix = viewMatrix * projectionMatrix;
+		Matrix44 viewportMatrix = MakeViewportMatrix(0.0f, 0.0f, float(kWindowWidth), float(kWindowHeight));
+
+		if (IsCollision(obb, segment)) {
+			color = 0xFF0000FF;
+		}else {
+			color = 0xFFFFFFFF;
+		}
+
+		Vec3 start = Transform(segment.origin, viewProjectionMatrix * viewportMatrix);
+		Vec3 end = Transform(segment.origin + segment.diff, viewProjectionMatrix * viewportMatrix);
+
+		ImGui::Begin("Camera");
+		ImGui::DragFloat3("CameraTranslate", &cameraTranslate.x, 0.01f);
+		ImGui::DragFloat3("CameraRotate", &cameraRotate.x, 0.01f);
+		ImGui::End();
+
+		ImGui::Begin("Window");
+		ImGui::DragFloat3("obb center", &obb.center.x, 0.01f);
+		ImGui::DragFloat("rotateX", &rotate.x, 0.01f);
+		ImGui::DragFloat("rotateY", &rotate.y, 0.01f);
+		ImGui::DragFloat("rotateZ", &rotate.z, 0.01f);
+		ImGui::DragFloat3("obb orientationX", &obb.orientation[0].x, 0.01f);
+		ImGui::DragFloat3("obb orientationY", &obb.orientation[1].x, 0.01f);
+		ImGui::DragFloat3("obb orientationZ", &obb.orientation[2].x, 0.01f);
+		ImGui::DragFloat3("obb size", &obb.size.x, 0.01f);
+
+		ImGui::DragFloat3("Segment origin", &segment.origin.x, 0.01f);
+		ImGui::DragFloat3("Segment diff", &segment.diff.x, 0.01f);
+		ImGui::End();
+
 		///
 		/// ↑更新処理ここまで
 		///
@@ -32,6 +101,11 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+		DrawGrid(viewProjectionMatrix, viewportMatrix);
+
+		DrawOBB(obb, viewProjectionMatrix, viewportMatrix, color);
+		Novice::DrawLine((int)start.x, (int)start.y, (int)end.x, (int)end.y, 0xFFFFFFFF);
 
 		///
 		/// ↑描画処理ここまで
